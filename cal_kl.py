852be7e54a3660029c2b1bf1df9552abb04e96f7
Apollo <hanlin@apollo.cs.rpi.edu>
2014-10-29 13:21:09 -0400

    Bug fixes for cal_kl.py

--- cal_kl.py
+++ cal_kl.py
@@ -1,14 +1,18 @@
 import lib.an_util as util
 import sys
 from os import listdir
+from os.path import join
 import math
+import numpy as np
 
 MSG = ("Calculate the Kullback-Liebler divergence "
        "for all hashtags in given directory "
        "containing files of daily counts.\n"
        "usage: {} hashtag_directory output_file_name")
-def cal_kl(ht_file, ht_kl):
-    ht_set, ht_dict = util.load_name_val_pairs(ht_file)
+
+def cal_kl(directory, ht_file, ht_kl):
+    ht_set, ht_dict = util.load_name_val_pairs(
+	join(directory, ht_file))
     days = len(ht_dict)
     kl = 0
     uniform = float(1)/days
@@ -23,11 +27,10 @@ def cal_kl(ht_file, ht_kl):
     ht_kl[ht_file] = kl
 
 if __name__ == '__main__':
-    directory, out_fn = util.check_input(sys.argv, 1, MSG)
+    directory, out_fn = util.check_input(sys.argv, 2, MSG)
 
     ht_kl = {}
     for ht_file in listdir(directory):
-        cal_kl(ht_file, ht_kl)
+        cal_kl(directory, ht_file, ht_kl)
     sorted_tup_ls = util.sort_dict(ht_kl)
     util.write_tup_ls(sorted_tup_ls, out_fn)
-
